a47d890d60f437f96589445d279f2ac146053fe8
941477276@qq.com <941477276@qq.com>
2022-03-23 21:54:41 +0800

    useClickOutside支持多个元素

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,10 +1,17 @@
 import { ref, onMounted, onBeforeUnmount, Ref } from 'vue';
 import { util } from '@/common/util';
 
-export function useClickOutside (eleRef: Ref<HTMLElement|null>, callback?: (isClickOutside: boolean) => void):Ref {
+export function useClickOutside (eleRefs: Ref<HTMLElement|null>|Ref<HTMLElement|null>[], callback?: (isClickOutside: boolean) => void):Ref {
   let flag = ref(false);
   let documentClickEvt = function (evt: MouseEvent) {
-    flag.value = !util.elementContains(eleRef.value, evt.target);
+    let target = evt.target;
+    if (Array.isArray(eleRefs)) {
+      flag.value = !eleRefs.some(function (refItem) {
+        return util.elementContains(refItem.value, target);
+      });
+    } else {
+      flag.value = !util.elementContains(eleRefs.value, target);
+    }
     // console.log('document click event', flag.value);
     if (typeof callback == 'function') {
       callback(flag.value);
