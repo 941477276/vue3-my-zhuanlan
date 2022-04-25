5ca92b4c37f277e07f65672562ee5495971e6d5b
941477276@qq.com <941477276@qq.com>
2022-04-25 22:16:10 +0800

    完善<bs-message-box>组件样式

--- src/components/bootstrap/bs-message-box/BsMessageBox.vue
+++ src/components/bootstrap/bs-message-box/BsMessageBox.vue
@@ -1,13 +1,20 @@
 <template>
 <div class="bs-message-box-wrapper" role="dialog" aria-modal="true">
-  <div class="bs-message-box theme-dialog">
-    <div class="bs-message-box-header">
-      <div class="bs-message-box-title">温馨提示</div>
-    </div>
+  <div class="bs-message-box">
+    <!--<div class="bs-message-box-header has-close">
+      <div class="bs-message-box-title">温馨提示温馨提示温馨提示温馨提示温馨提示温馨提示温馨提示</div>
+      <bs-button class="bs-message-box-close" type="link" aria-label="close">
+        <span>×</span>
+      </bs-button>
+    </div>-->
     <div class="bs-message-box-body">
-      <p>一个消息弹窗！</p>
-      <p>一个消息弹窗！</p>
-      <p>一个消息弹窗！</p>
+      <span class="bs-message-box-icon">
+         <BsIcon name="check-circle-fill"></BsIcon>
+      </span>
+      <div class="bs-message-box-content-wrap">
+        <div class="bs-message-box-title">温馨提示温馨提示温馨提示温馨提示温馨提示温馨提示温馨提示</div>
+        <div class="bs-message-box-content">message box内容区域！message box内容区域！message box内容区域！message box内容区域！message box内容区域！</div>
+      </div>
     </div>
     <div class="bs-message-box-btns">
       <bs-button type="primary" plain>取消</bs-button>
@@ -23,11 +30,21 @@ import {
   ref
 } from 'vue';
 import BsButton from '../bs-button/BsButton.vue';
+import BsIcon from '../bs-icon/BsIcon.vue';
+import { MessageType } from '@/ts-tokens/bootstrap/message';
 
+const defaultIconMap: Record<MessageType, string> = {
+  info: 'info-circle-fill',
+  success: 'check-circle-fill',
+  warning: 'exclamation-circle-fill',
+  danger: 'x-circle-fill'
+};
+let messageBoxCount = 0;
 export default defineComponent({
   name: 'BsMessageBox',
   components: {
-    BsButton
+    BsButton,
+    BsIcon
   }
 });
 </script>
