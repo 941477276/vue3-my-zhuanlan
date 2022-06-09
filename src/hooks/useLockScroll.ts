7b5455ea8dc37808354b66d0fd2d5246b14f1a7a
liyn <liyn@myibc.net>
2022-06-09 17:38:08 +0800

    解决 useLockScroll 解锁滚动条的bug

--- src/hooks/useLockScroll.ts
+++ src/hooks/useLockScroll.ts
@@ -26,26 +26,25 @@ export function useLockScroll () {
   if (hasScroll.horizontal) {
     body.style.paddingBottom = scrollWidth.horizontal + 'px';
   }
-
   // 返回一个解除锁定滚动条的函数
   return function () {
     let body = document.body;
     if (!originBodyHasLockClass) {
       util.removeClass(body, 'bs-lock-scroll');
     }
-    if (!originBodyOverflow && originBodyOverflow !== 'hidden') {
+    if (originBodyOverflow) {
       body.style.overflow = originBodyOverflow;
     } else {
       body.style.overflow = ''; // 移除body上的overflow属性
     }
 
-    if (!originBodyPaddingRight && parseFloat(originBodyPaddingRight) !== scrollWidth.vertical) {
+    if (originBodyPaddingRight && parseFloat(originBodyPaddingRight) !== scrollWidth.vertical) {
       body.style.paddingRight = originBodyPaddingRight;
     } else {
       body.style.paddingRight = ''; // 移除body上的paddingRight属性
     }
 
-    if (!originBodyPaddingBottom && parseFloat(originBodyPaddingBottom) !== scrollWidth.horizontal) {
+    if (originBodyPaddingBottom && parseFloat(originBodyPaddingBottom) !== scrollWidth.horizontal) {
       body.style.paddingBottom = originBodyPaddingBottom;
     } else {
       body.style.paddingBottom = ''; // 移除body上的paddingBottom属性
