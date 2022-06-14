2c022d9c65af435c2505678ece5de223c58d9d28
liyn <liyn@myibc.net>
2022-06-14 18:39:40 +0800

    添加<bs-collapse-transition>组件

---
+++ src/components/bootstrap/bs-collapse-transition/BsCollapseTransition.vue
@@ -0,0 +1,44 @@
+<template>
+  <transition
+    :name="transitionName"
+    @before-enter="collapseBeforeEnter"
+    @enter="collapseEnter"
+    @after-enter="collapseAfterEnter"
+    @before-leave="collapseBeforeLeave"
+    @leave="collapseLeave"
+    @after-leave="collapseAfterLeave">
+    <slot></slot>
+  </transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useCollapseTransition } from '@/hooks/useCollapseTransition';
+
+/**
+ * 元素折叠过度效果
+ */
+export default defineComponent({
+  name: 'BsCollapseTransition',
+  props: {
+    transitionName: {
+      type: String,
+      default: 'collapse-transition'
+    }
+  },
+  setup () {
+    let transitionEvents = useCollapseTransition();
+
+    return {
+      ...transitionEvents
+    };
+  }
+});
+</script>
+
+<style lang="scss">
+.collapse-transition-enter-active,
+.collapse-transition-leave-active{
+  transition: height .3s ease-in-out, padding .3s ease-in-out, max-height .3s ease-in-out;
+}
+</style>
