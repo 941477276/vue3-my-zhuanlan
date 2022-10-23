e204e21b535507993edd8c3c233b6c0d45e137c1
拾晚霞 <941477276@qq.com>
2022-10-23 20:52:45 +0800

    <bs-menu>组件，新增菜单收起来后hover在子菜单上显示tooltip功能

--- src/components/bootstrap/bs-menu/widgets/BsMenuItem.vue
+++ src/components/bootstrap/bs-menu/widgets/BsMenuItem.vue
@@ -1,27 +1,61 @@
 <template>
+  <BsTooltip
+    v-if="parentIsMenuRoot && isCollapsed"
+    :disabled="!isCollapsed"
+    :content="tooltipContent"
+    placement="right"
+    transition-name="scale">
+    <li
+      class="bs-menu-item"
+      :class="{
+        'has-icon': icon || $slots.icon,
+        'is-disabled': disabled
+      }"
+      :style="{
+        paddingLeft: paddingLeft.value ? (paddingLeft.value + paddingLeft.unit): ''
+      }"
+      role="menuitem"
+      :aria-level="keyIndexPath.length"
+      :aria-disabled="disabled"
+      @mouseenter="handleMouseenter">
+      <span
+        v-if="icon || $slots.icon"
+        class="bs-menu-item-icon"
+        role="img">
+        <slot name="icon">
+          <BsIcon :name="icon"></BsIcon>
+        </slot>
+      </span>
+      <span class="bs-menu-item-title">
+        <slot>{{ title }}</slot>
+      </span>
+    </li>
+  </BsTooltip>
   <li
+    v-else
     class="bs-menu-item"
     :class="{
-      'has-icon': icon || $slots.icon,
-      'is-disabled': disabled
-    }"
+        'has-icon': icon || $slots.icon,
+        'is-disabled': disabled
+      }"
     :style="{
-      paddingLeft: paddingLeft.value ? (paddingLeft.value + paddingLeft.unit): ''
-    }"
+        paddingLeft: paddingLeft.value ? (paddingLeft.value + paddingLeft.unit): ''
+      }"
     role="menuitem"
     :aria-level="keyIndexPath.length"
-    :aria-disabled="disabled">
-    <span
-      v-if="icon || $slots.icon"
-      class="bs-menu-item-icon"
-      role="img">
-      <slot name="icon">
-        <BsIcon :name="icon"></BsIcon>
-      </slot>
-    </span>
-    <span class="bs-menu-item-title">
-      <slot>{{ title }}</slot>
-    </span>
+    :aria-disabled="disabled"
+    @mouseenter="handleMouseenter">
+      <span
+        v-if="icon || $slots.icon"
+        class="bs-menu-item-icon"
+        role="img">
+        <slot name="icon">
+          <BsIcon :name="icon"></BsIcon>
+        </slot>
+      </span>
+      <span class="bs-menu-item-title">
+        <slot>{{ title }}</slot>
+      </span>
   </li>
 </template>
 
@@ -29,16 +63,21 @@
 import {
   defineComponent,
   getCurrentInstance,
-  computed
+  computed,
+  inject,
+  ref
 } from 'vue';
+import BsTooltip from '../../bs-tooltip/BsTooltip.vue';
 import BsIcon from '../../bs-icon/BsIcon.vue';
 import { useMenuLevel } from '../hooks/useMenuLevel';
+import { bsMenuRootInjectKey } from '@/ts-tokens/bootstrap/menu';
 
 let menuItemCount = 0;
 export default defineComponent({
   name: 'BsMenuItem',
   components: {
-    BsIcon
+    BsIcon,
+    BsTooltip
   },
   props: {
     keyIndex: { // 唯一标识，必填
@@ -62,6 +101,12 @@ export default defineComponent({
   setup (props: any, ctx: any) {
     let currentIns = getCurrentInstance()!;
     let menuItemId = `bs-menu-item_${++menuItemCount}`;
+    // 根菜单提供的上下文
+    let menuRootCtx = inject(bsMenuRootInjectKey) as any;
+
+    let isCollapsed = computed(function () {
+      return menuRootCtx?.props.collapse;
+    });
 
     // 获取当前组件的父级菜单组件，层级路径，层级ID
     let {
@@ -71,11 +116,32 @@ export default defineComponent({
       paddingLeft
     } = useMenuLevel(currentIns, props, menuItemId);
 
+    // 判断组件的父级组件是否为<bs-menu>组件
+    let parentIsMenuRoot = computed(function () {
+      let parent = parentMenu.value;
+      if (!parent) {
+        return false;
+      }
+      return parent.type.name == 'BsMenu';
+    });
+
+    let tooltipContent = ref('');
+    let handleMouseenter = function (evt: MouseEvent) {
+      let target = evt.target as HTMLElement;
+      let titleEl = target.querySelector('.bs-menu-item-title');
+      tooltipContent.value = (titleEl as any)?.innerText || titleEl?.textContent;
+    };
+
     return {
       comId: menuItemId,
       currentKeyIndex,
       keyIndexPath,
-      paddingLeft
+      paddingLeft,
+      isCollapsed,
+      parentIsMenuRoot,
+      tooltipContent,
+
+      handleMouseenter
     };
   }
 });
