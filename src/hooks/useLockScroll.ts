31a3fb6ac0085ede9db6e9d5233cc67a3c4a50a9
liyn <liyn@myibc.net>
2022-11-16 14:07:21 +0800

    useLockScroll，工具函数从bs-utils.ts中导入

--- src/hooks/useLockScroll.ts
+++ src/hooks/useLockScroll.ts
@@ -1,6 +1,10 @@
 import {
-  util
-} from '@/common/util';
+  hasClass,
+  hasScroll,
+  scrollWidth,
+  addClass,
+  removeClass
+} from '@/common/bs-util';
 
 /**
  * 锁定浏览器滚动条
@@ -11,24 +15,24 @@ export function useLockScroll () {
   let originBodyOverflow = body.style.overflow;
   let originBodyPaddingRight = body.style.paddingRight;
   let originBodyPaddingBottom = body.style.paddingBottom;
-  let originBodyHasLockClass = util.hasClass(body, 'bs-lock-scroll');
-  let hasScroll = util.hasScroll();
+  let originBodyHasLockClass = hasClass(body, 'bs-lock-scroll');
+  let documentHasScroll = hasScroll();
   // @ts-ignore
-  let scrollWidth = util.scrollWidth();
+  let documentScrollWidth = scrollWidth();
   // 标记本次是否锁定了页面
   let locked = false;
 
   if (!originBodyHasLockClass) {
-    util.addClass(body, 'bs-lock-scroll');
+    addClass(body, 'bs-lock-scroll');
   }
   if (originBodyOverflow != 'hidden') {
     body.style.overflow = 'hidden';
     locked = true;
-    if (hasScroll.vertical) {
-      body.style.paddingRight = scrollWidth.vertical + 'px';
+    if (documentHasScroll.vertical) {
+      body.style.paddingRight = documentScrollWidth.vertical + 'px';
     }
-    if (hasScroll.horizontal) {
-      body.style.paddingBottom = scrollWidth.horizontal + 'px';
+    if (documentHasScroll.horizontal) {
+      body.style.paddingBottom = documentScrollWidth.horizontal + 'px';
     }
   }
 
@@ -36,7 +40,7 @@ export function useLockScroll () {
   return function () {
     let body = document.body;
     if (!originBodyHasLockClass) {
-      util.removeClass(body, 'bs-lock-scroll');
+      removeClass(body, 'bs-lock-scroll');
     }
     if (!locked) {
       return;
@@ -47,13 +51,13 @@ export function useLockScroll () {
       body.style.overflow = ''; // 移除body上的overflow属性
     }
 
-    if (originBodyPaddingRight && parseFloat(originBodyPaddingRight) !== scrollWidth.vertical) {
+    if (originBodyPaddingRight && parseFloat(originBodyPaddingRight) !== documentScrollWidth.vertical) {
       body.style.paddingRight = originBodyPaddingRight;
     } else {
       body.style.paddingRight = ''; // 移除body上的paddingRight属性
     }
 
-    if (originBodyPaddingBottom && parseFloat(originBodyPaddingBottom) !== scrollWidth.horizontal) {
+    if (originBodyPaddingBottom && parseFloat(originBodyPaddingBottom) !== documentScrollWidth.horizontal) {
       body.style.paddingBottom = originBodyPaddingBottom;
     } else {
       body.style.paddingBottom = ''; // 移除body上的paddingBottom属性
