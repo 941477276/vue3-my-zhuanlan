a8c9151275b52b90d9d57a83dfbfcfe0505f7dd5
拾晚霞 <941477276@qq.com>
2022-11-17 21:15:52 +0800

    <bs-backtop>组件，样式抽取到单独到样式文件中

--- src/App.vue
+++ src/App.vue
@@ -61,9 +61,9 @@
     <!--<div class="box">
       <BsMaskUsage></BsMaskUsage>
     </div>-->
-    <div class="box">
+    <!--<div class="box">
       <BsAlertUsage></BsAlertUsage>
-    </div>
+    </div>-->
     <!--<div class="box">
       <BsInputNumberUsage></BsInputNumberUsage>
     </div>-->
@@ -112,9 +112,9 @@
     <!--<div class="box">
       <BsDatePickerUsage></BsDatePickerUsage>
     </div>-->
-    <!--<div class="box">
+    <div class="box">
       <BsBackTopUsage></BsBackTopUsage>
-    </div>-->
+    </div>
     <!--<div class="box">
       <BsRelativeTimeUsage></BsRelativeTimeUsage>
     </div>-->
