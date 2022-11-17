4e73a915d79d2b92dbc9e38a9949d294400408fc
拾晚霞 <941477276@qq.com>
2022-11-17 21:06:50 +0800

    删除 bs-dropdown2 文件夹

--- src/components/bootstrap/bs-dropdown2/index.ts
+++
@@ -1,9 +0,0 @@
-import BsDropdown from './BsDropdown.vue';
-import BsDropdownItem from './widgets/BsDropdownItem.vue';
-
-BsDropdown.install = function (vueApp: any) {
-  vueApp.component(BsDropdown.name, BsDropdown);
-  vueApp.component(BsDropdownItem.name, BsDropdownItem);
-};
-
-export default BsDropdown;
