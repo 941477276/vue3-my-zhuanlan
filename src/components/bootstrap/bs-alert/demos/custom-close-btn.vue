9aa0f846460e07336d82ab1ef8d966cfd1f40dce
拾晚霞 <941477276@qq.com>
2022-12-28 21:00:54 +0800

    <alert>组件示例编写

---
+++ src/components/bootstrap/bs-alert/demos/custom-close-btn.vue
@@ -0,0 +1,30 @@
+<docs>
+---
+order: 0
+title:
+  zh-CN: 自定义关闭按钮
+  en-US: Custom close button
+---
+
+## zh-CN
+自定义关闭按钮
+
+## en-US
+custom close button
+</docs>
+
+<template>
+  <div>
+    <BsAlert
+      title="自定义关闭按钮！"
+      type="primary"
+      closeable
+      close-text="知道啦"></BsAlert>
+
+    <BsAlert
+      title="超长内容！超长内容！超长内容！超长内容！超长内容！超长内容！超长内容！超长内容！超长内容！超长内容！超长内容！超长内容！"
+      type="danger"
+      closeable
+      close-text="知道啦"></BsAlert>
+  </div>
+</template>
