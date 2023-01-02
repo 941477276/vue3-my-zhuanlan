3a450e8c00042dde132277f2d5d2b0dcb51158d2
拾晚霞 <941477276@qq.com>
2023-01-02 15:28:09 +0800

    <date-picker>组件示例编写

--- src/components/bootstrap/bs-date-picker/BsDatePickerUsage.vue
+++ src/components/bootstrap/bs-date-picker/BsDatePickerUsage.vue
@@ -5,8 +5,10 @@
         v-model:value="value1"
         value-format="YYYY-MM-DD"
         show-time
+        :defaultPickerValue="now"
         :disabledDate="disabledDate"
         :date-render="dateRender"/>
+      <bs-button @click="updateNow">修改defaultPickerValue</bs-button>
       <a-date-picker v-model:value="value2" picker="week" />
       <a-date-picker v-model:value="value3" picker="month" />
       <a-date-picker v-model:value="value4" picker="quarter" data-value-format="YYYY-[Q]Q" />
@@ -14,132 +16,38 @@
     </a-space>
     <div>
       <h3>基本使用</h3>
-      <BsDatePicker name="startDate" v-model="date1">
-        <!-- <bs-button>触发按钮</bs-button> -->
-      </BsDatePicker>
+      <Basic></Basic>
     </div>
 
     <div>
       <hr>
       <h3>选择时间</h3>
-      <BsDatePicker v-model="date5" picker-type="dateTime"></BsDatePicker>
-    </div>
-
-     <div>
-      <hr>
-      <h3>选择时间（12小时值）</h3>
-      <BsDatePicker v-model="date6" picker-type="dateTime" value-format="YYYY-MM-DD hh:mm:ss a" :time-panel-props="{ use12Hours: true, }"></BsDatePicker>
+      <DateTime></DateTime>
     </div>
 
     <div>
       <hr>
       <h3>自定义渲染</h3>
-      <h6 style="margin: 0.5rem 0;">使用 <code>dateRender</code> 函数</h6>
-      <BsDatePicker
-        name="startDate"
-        v-model="date1"
-        :date-render="dateRender2"></BsDatePicker>
-      <h6 style="margin: 0.5rem 0;">使用 <code>#dateRender</code> 插槽</h6>
-      <BsDatePicker
-        name="startDate"
-        v-model="date1">
-        <template #dateRender="cell">
-          {{ cell.dayjsIns.date() }}<small style="color: #f60;margin-left: 2px; opacity: 0.6;">cn</small>
-        </template>
-      </BsDatePicker>
+      <CustomCell></CustomCell>
     </div>
 
     <div>
       <hr>
       <h3>禁用</h3>
-      <BsDatePicker v-model="date2" :disabled-date="disabledDate2"></BsDatePicker>
+      <Disabled></Disabled>
     </div>
 
     <div>
       <hr>
       <h3>带快捷按钮</h3>
-      <h6>快捷按钮在左边</h6>
-      <BsDatePicker v-model="date4" show-sidebar>
-        <template #sidebar="{date}">
-          <div>
-            <bs-button type="">今天{{ date && date.format('YYYY-MM-DD') }}</bs-button>
-          </div>
-        </template>
-      </BsDatePicker>
-
-      <h6 style="margin-top: 1rem;">快捷按钮在右边</h6>
-      <BsDatePicker v-model="date4" show-sidebar sidebar-in-right>
-        <template #sidebar="{date}">
-          <div>
-            <bs-button type="">今天{{ date && date.format('YYYY-MM-DD') }}</bs-button>
-          </div>
-        </template>
-      </BsDatePicker>
+      <Sidebar></Sidebar>
     </div>
 
     <div>
       <hr>
-      <h3>选择周</h3>
-      <BsDatePicker picker-type="week" v-model="week1"></BsDatePicker>
+      <h3>默认显示日期</h3>
+      <PanelViewDate></PanelViewDate>
     </div>
-
-    <div>
-      <hr>
-      <h3>选择周（默认有值）</h3>
-      <BsDatePicker picker-type="week" v-model="week2" :disabled-date="disabledWeek"></BsDatePicker>
-    </div>
-
-     <div>
-      <hr>
-      <h3>选择月份</h3>
-      <BsDatePicker
-        v-model="date3"
-        picker-type="month"
-        data-disabled-date="(date) => {return date.month() < 7}"></BsDatePicker>
-    </div>
-
-    <div>
-      <hr>
-      <h3>选择季度</h3>
-      <BsDatePicker
-        v-model="quarterDate"
-        picker-type="quarter"></BsDatePicker>
-    </div>
-
-   <div>
-     <hr>
-     <h3>选择季度(默认有值)</h3>
-     <BsDatePicker
-       v-model="quarterDate2"
-       picker-type="quarter"
-       value-format="YYYY-[Q]Q"></BsDatePicker>
-   </div>
-
-   <div>
-     <hr>
-     <h3>选择年份</h3>
-     <BsDatePicker
-       v-model="year"
-       picker-type="year"></BsDatePicker>
-   </div>
-
-   <div>
-     <hr>
-     <h3>选择年份(默认有值)</h3>
-     <BsDatePicker
-       v-model="year2"
-       format="YYYY年"
-       picker-type="year"
-       :disabled-date="disabledYear"></BsDatePicker>
-   </div>
-
-   <div>
-     <hr>
-     <h3>选择年份区间</h3>
-     <BsDatePicker
-       v-model="decade"
-       picker-type="decade"></BsDatePicker>
-   </div>
   </div>
 </template>
 
@@ -151,11 +59,23 @@ import {
 } from 'vue';
 import BsDatePicker from './BsDatePicker';
 import dayjs, { Dayjs } from 'dayjs';
+import Basic from './demos/basic.vue';
+import DateTime from './demos/date-time.vue';
+import CustomCell from './demos/custom-cell.vue';
+import Disabled from './demos/disabled.vue';
+import Sidebar from './demos/sidebar.vue';
+import PanelViewDate from './demos/panl-view-date.vue';
 
 export default defineComponent({
   name: 'BsDatePickerUsage',
   components: {
-    BsDatePicker
+    // BsDatePicker,
+    Basic,
+    DateTime,
+    CustomCell,
+    Disabled,
+    Sidebar,
+    PanelViewDate
   },
   setup () {
     let disabledDate = function (currentDate: Dayjs) {
@@ -163,6 +83,7 @@ export default defineComponent({
       // console.log('day', day);
       return day >= 1 && day <= 10;
     };
+    let now = ref(dayjs().year(2021));
     let date1 = ref('2022-08-10');
     // let date1 = ref('');
     let date2 = ref();
@@ -188,7 +109,7 @@ export default defineComponent({
     // console.log('week date2', dayjs('2022-01-01').week(33));
     // console.log('aaAaaaaaa', dayjs('2022-08-20 19:37:50 AM', 'YYYY-MM-DD hh:mm:ss am', true).isValid());
     return {
-      value1: ref<Dayjs>(),
+      value1: ref<Dayjs>(dayjs()),
       value2: ref<Dayjs>(),
       value3: ref<Dayjs>(),
       value4: ref<Dayjs>(dayjs()),
@@ -202,6 +123,10 @@ export default defineComponent({
       date6,
       quarterDate,
       quarterDate2,
+      now,
+      updateNow () {
+        now.value = dayjs().year(2016);
+      },
 
       year,
       year2,
