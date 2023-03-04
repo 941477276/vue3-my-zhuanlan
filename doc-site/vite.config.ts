819d1aaa5ca2a18269447298e878b2ca8637a8df
拾晚霞 <941477276@qq.com>
2023-03-04 11:26:08 +0800

    解决不支持tsx语法问题

--- doc-site/vite.config.ts
+++ doc-site/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
     port: 9527,
     open: true
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    })
+  ]
 });
