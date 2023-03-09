df8746ab9a11a326b678d96aa84008dd38977efb
liyn <liyn@myibc.net>
2023-03-09 18:26:50 +0800

    完善<demo-box>组件

---
+++ doc-site/src/common/utils.ts
@@ -0,0 +1,49 @@
+/**
+ * 复制文本
+ * @param value 文本值
+ * @returns {number} 返回1表示复制成功
+ */
+export function copyText (value: string | number) {
+  if (!value) {
+    return -1;
+  }
+  if (!document.execCommand) {
+    // Message.error('浏览器不支持')
+    return -2;
+  }
+  try {
+    const selectText = (
+      textbox: any,
+      startIndex: number,
+      stopIndex: number
+    ) => {
+      if (textbox.createTextRange) {
+        // ie
+        const range = textbox.createTextRange();
+        range.collapse(true);
+        range.moveStart('character', startIndex); // 起始光标
+        range.moveEnd('character', stopIndex - startIndex); // 结束光标
+        range.select(); // 不兼容苹果
+      } else {
+        // firefox/chrome
+        textbox.setSelectionRange(startIndex, stopIndex);
+        textbox.focus();
+      }
+    };
+    const copyEl = document.createElement('input');
+    copyEl.value = value as string;
+    copyEl.setAttribute('readonly', 'readonly');
+    copyEl.setAttribute('class', 'copy-dpape');
+    document.body.appendChild(copyEl);
+    selectText(copyEl, 0, (value + '').length);
+    if (document.execCommand('copy')) {
+      copyEl.blur();
+      document.execCommand('copy');
+      document.body.removeChild(copyEl);
+      return 1;
+    }
+  } catch (e) {
+    console.log(e);
+    return 2;
+  }
+}
--- doc-site/src/components/DemoBox.vue
+++ doc-site/src/components/DemoBox.vue
@@ -1,48 +1,68 @@
 <template>
 <div class="demo-box">
   <div class="demo-box-header">
-    <h6 class="demo-title">按钮类型</h6>
+    <h6 class="demo-title">按钮类型{{ title.cn }}</h6>
     <div class="demo-operate-area">
-      <bs-button size="sm">
-        <BsiPlayFill></BsiPlayFill>
-      </bs-button>
-      <bs-button size="sm">
-        <BsiFiles></BsiFiles>
-      </bs-button>
-      <bs-button size="sm">
-        <BsiCode></BsiCode>
-        <!--<BsiCodeSlash></BsiCodeSlash>-->
-      </bs-button>
+      <bs-tooltip content="在Playground中打开" placement="top" transition-name="scale">
+        <bs-button size="sm">
+          <BsiPlayFill></BsiPlayFill>
+        </bs-button>
+      </bs-tooltip>
+      <bs-tooltip content="复制代码" placement="top" transition-name="scale">
+        <bs-button size="sm" @click="copyExampleCode">
+          <BsiFiles></BsiFiles>
+        </bs-button>
+      </bs-tooltip>
+      <bs-tooltip :content="codeExpanded ? '收起代码' : '展开代码'" placement="top" transition-name="scale">
+        <bs-button size="sm" @click="codeExpanded = !codeExpanded"
+                   :class="{
+                     'example-code-expanded': codeExpanded
+                   }">
+          <BsiCode></BsiCode>
+        </bs-button>
+      </bs-tooltip>
     </div>
   </div>
-  <div class="demo-description">
-    理发师的法律解释道 点击索科洛夫家时离开
-  </div>
+  <div class="demo-description" v-html="description.cn"></div>
   <div class="demo-example">
-    <bs-button>default</bs-button>
-    <bs-button type="primary">primary</bs-button>
-    <bs-button type="secondary">secondary</bs-button>
-    <bs-button type="success">success</bs-button>
-    <bs-button type="warning">warning</bs-button>
-    <bs-button type="danger">danger</bs-button>
-    <bs-button type="info">info</bs-button>
-    <bs-button type="light">light</bs-button>
-    <bs-button type="danger">danger</bs-button>
+    <slot>
+      <bs-button>default</bs-button>
+      <bs-button type="primary">primary</bs-button>
+      <bs-button type="secondary">secondary</bs-button>
+      <bs-button type="success">success</bs-button>
+      <bs-button type="warning">warning</bs-button>
+      <bs-button type="danger">danger</bs-button>
+      <bs-button type="info">info</bs-button>
+      <bs-button type="light">light</bs-button>
+      <bs-button type="danger">danger</bs-button>
+    </slot>
   </div>
-  <div class="demo-example-code">
-    <pre>这是一段代码</pre>
+  <div class="demo-example-code" v-show="codeExpanded">
+    <pre v-pre>
+      这是一段代码
+      这是一段代码
+      这是一段代码这是一段代码
+      这是一段代码
+    </pre>
+    <div class="shrink-code-operate">
+      <bs-button block @click="codeExpanded = false"><BsiChevronUp></BsiChevronUp>收起代码</bs-button>
+    </div>
   </div>
 </div>
 </template>
 
 <script>
 import {
+  ref,
   defineComponent
 } from 'vue';
 import { BsiPlayFill } from 'vue3-bootstrap-icon/es/icons/BsiPlayFill';
 import { BsiCode } from 'vue3-bootstrap-icon/es/icons/BsiCode';
 // import { BsiCodeSlash } from 'vue3-bootstrap-icon/es/icons/BsiCodeSlash';
 import { BsiFiles } from 'vue3-bootstrap-icon/es/icons/BsiFiles';
+import { BsiChevronUp } from 'vue3-bootstrap-icon/es/icons/BsiChevronUp';
+import { copyText } from '../common/utils';
+import { BsMessage } from '../../../src/components/bs-message';
 
 export default defineComponent({
   name: 'DemoBox',
@@ -50,7 +70,46 @@ export default defineComponent({
     BsiPlayFill,
     BsiCode,
     // BsiCodeSlash,
-    BsiFiles
+    BsiFiles,
+    BsiChevronUp
+  },
+  props: {
+    langCode: { // 语言编码
+      type: String,
+      default: 'cn'
+    },
+    title: { // 标题
+      type: Object,
+      default () {
+        return {
+          cn: '',
+          en: ''
+        };
+      }
+    },
+    description: { // 描述
+      type: Object,
+      default () {
+        return {
+          cn: '',
+          en: ''
+        };
+      }
+    },
+    exampleCode: { // 示例代码
+      type: String,
+      default: ''
+    }
+  },
+  setup () {
+    let codeExpanded = ref(false);
+
+    return {
+      codeExpanded,
+      copyExampleCode () { // 复制代码
+        BsMessage.success('代码已复制');
+      }
+    };
   }
 });
 </script>
