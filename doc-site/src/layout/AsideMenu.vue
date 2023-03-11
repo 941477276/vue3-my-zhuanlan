d0708922d78fb12ab166d1b9409142a913094b8a
拾晚霞 <941477276@qq.com>
2023-03-11 21:03:07 +0800

    完善左侧菜单

--- doc-site/src/layout/AsideMenu.vue
+++ doc-site/src/layout/AsideMenu.vue
@@ -4,16 +4,20 @@
       <bs-input class="header-search-input" placeholder="搜索文档..."></bs-input>
     </div>
     <div class="aside-menu-container">
-      <bs-menu mode="vertical">
-        <bs-menu-item-group key-index="tongyong" title="通用">
-          <bs-menu-item key-index="button">按钮</bs-menu-item>
-          <bs-menu-item key-index="icon">Icon图标</bs-menu-item>
-        </bs-menu-item-group>
-        <bs-menu-item-group key-index="data-input" title="数据录入">
-          <bs-menu-item key-index="input">输入框</bs-menu-item>
-          <bs-menu-item key-index="select">选择器</bs-menu-item>
-          <bs-menu-item key-index="checkbox">复选框</bs-menu-item>
-          <bs-menu-item key-index="radio">单选框</bs-menu-item>
+      <bs-menu mode="vertical" v-model:selected-keys="activeMenu">
+        <bs-menu-item-group
+          v-for="group in menus"
+          :key="group.type"
+          :key-index="group.type"
+          :title="group.type">
+          <bs-menu-item
+            v-for="menu in group.children"
+            :key="menu.componentName"
+            :key-index="menu.componentName"
+            @click="handleMenuItemClick(menu)">
+            {{ menu.title }}
+            <small v-if="menu.subtitle">{{ menu.subtitle }}</small>
+          </bs-menu-item>
         </bs-menu-item-group>
       </bs-menu>
     </div>
@@ -22,11 +26,37 @@
 
 <script lang="ts">
 import {
-  defineComponent
+  defineComponent,
+  ref,
+  computed
 } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import menuZhCN from '../router/menu.zh-CN';
 
 export default defineComponent({
-  name: 'AsideMenu'
+  name: 'AsideMenu',
+  setup () {
+    let menus = ref(menuZhCN);
+    let router = useRouter();
+    let route = useRoute();
+    let activeMenu = computed({
+      get () {
+        console.log('route.currentRoute', route);
+        return [route.name];
+      },
+      set (newActiveMenu) {
+        console.log('set newActiveMenu', newActiveMenu);
+      }
+    });
+
+    return {
+      menus,
+      activeMenu,
+      handleMenuItemClick (menuItem) {
+        router.push({ name: menuItem.componentName });
+      }
+    };
+  }
 });
 </script>
 
